30a34be0d721cdbf4b1bee0216c97f0ab179fd8b
Erbit <erbitservices@gmail.com>
2024-11-23 14:24:49 +0530

    mihir

--- src/assets/Components/Addbook.jsx
+++ src/assets/Components/Addbook.jsx
@@ -458,7 +458,7 @@ function Addbook({ schemename , backtodashboard }) {
                   <option>Select</option>
                   <option>Gujarati</option>
                   <option>Hindi</option>
-                  <option>Englixh</option>
+                  <option>English</option>
                 </select>
               </div>
               <div className="Addbook-inputarea">
@@ -566,20 +566,17 @@ function Addbook({ schemename , backtodashboard }) {
                   <tr>
                     {/* <th>Front Image</th> */}
                     {/* <th>Back Image</th> */}
+                    
+                    <th> ISBN</th>
                     <th>Book Name</th>
+                    
+                    <th> Author Name</th>
                     {/* <th>Book Name Guj</th> */}
                     <th>Price</th>
-                    <th> ISBN</th>
-                    <th> Category</th>
-                    {/* <th> Language</th> */}
-                    <th> PublisherName</th>
-                    {/* <th> Size</th> */}
+                    
                     <th> Subject</th>
-                    <th> Weight</th>
-                    {/* <th> Binding</th> */}
-                    <th> AuthorName</th>
-                    {/* <th> AuthorNameGuj</th> */}
-                    {/* <th> Discribption</th> */}
+                    <th> Category</th>
+                    
                     <th> Edit</th>
                     <th> Delete</th>
                   </tr>
@@ -605,19 +602,19 @@ function Addbook({ schemename , backtodashboard }) {
                             src={i.BackImage}
                           />
                         </td> */}
+                        
+                        <td key={i.ISBN}> {i.ISBN}</td>
                         <td key={i.BookName}> {i.BookName}</td>
-
+                        <td key={i.AuthorName}> {i.AuthorName}</td>
                         {/* <td key={i.BookNameGuj}> {i.BookNameGuj}</td> */}
                         <td key={i.Price}> {i.Price}</td>
-                        <td key={i.ISBN}> {i.ISBN}</td>
+                        <td key={i.Subject}> {i.Subject}</td>
                         <td key={i.Category}> {i.Category}</td>
                         {/* <td key={i.Language}> {i.Language}</td> */}
                         <td key={i.PublisherName}> {i.PublisherName}</td>
                         {/* <td key={i.Size}> {i.Size}</td> */}
-                        <td key={i.Subject}> {i.Subject}</td>
                         <td key={i.Weight}> {i.Weight}</td>
                         {/* <td key={i.Binding}> {i.Binding}</td> */}
-                        <td key={i.AuthorName}> {i.AuthorName}</td>
                         {/* <td key={i.AuthorNameGuj}> {i.AuthorNameGuj}</td> */}
                         {/* <td key={i.Discribption}> {i.Discribption}</td> */}
                         <td
